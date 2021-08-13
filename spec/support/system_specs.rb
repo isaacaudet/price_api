d6084715dc48e3effc4bca17e78a6dc4f01e7576
IsaacAudet <isaac.audet@gmail.com>
2021-08-13 16:28:49 -0700

    Initial commit

---
+++ spec/support/system_specs.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "capybara/rspec"
+
+Capybara.server = :puma, {Silent: true}
+
+Capybara.register_driver :chrome_headless do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--no-sandbox")
+  options.add_argument("--headless")
+  options.add_argument("--disable-gpu")
+  options.add_argument("--window-size=2800,2800")
+
+  if ENV["CHROME_URL"]
+    Capybara::Selenium::Driver.new(app,
+      browser: :remote,
+      url: ENV["CHROME_URL"],
+      options: options)
+  else
+    require "webdrivers"
+    Capybara::Selenium::Driver.new(app,
+      browser: :chrome,
+      options: options)
+  end
+end
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) {
+    driven_by :chrome_headless
+
+    capybara_host = IPSocket.getaddress(Socket.gethostname)
+
+    Capybara.app_host = "http://#{capybara_host}:3000"
+    Capybara.server_host = capybara_host
+    Capybara.server_port = 3000
+  }
+end
